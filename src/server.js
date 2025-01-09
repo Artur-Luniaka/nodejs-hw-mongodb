780f9b7a6b6c867085a8fdc13ab91d8f03789f31
Artur Luniaka <artur01049718@gmail.com>
2025-01-09 08:56:44 +0100

    finished steps 2 and 3 for hw-02

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = 3000;
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
@@ -15,6 +16,11 @@ export const setupServer = () => {
       },
     }),
   );
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
